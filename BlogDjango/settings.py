5959f8efe1efdae1662580bfa9b78ea6a92b547c
MatchaOnMuffins <henryzhangjcle@gmail.com>
2022-06-29 00:57:07 -0400

    changed debug mode in settings.py

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vep&a0z%u_wux*tc=m-#l1gvuz0ddsu-b_(7cq=qw0nnz238h^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
